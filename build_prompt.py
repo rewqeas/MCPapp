5bc959e980a7b4b11039701fa09f7f28a44e80d9
rewqeas <ashimneupane71@gmail.com>
2025-07-27 08:30:40 +0545

    second commit

--- build_prompt.py
+++ build_prompt.py
@@ -11,8 +11,10 @@ def build_mcp_prompt(system, tools, query, memory = []):
 
     prompt += (
         "Instructions:\n"
-        "If the query is about weather, respond only with a code block like this:\n"
+        "1.If the query is about weather, respond only with a code block like this:\n"
         "```tool_code\nget_weather(city='CityName')\n```\n"
+        "2. If the query is about exchange rates, respond only with a code block like this:\n"
+        "```tool_code\nget_exchange_rate(from_currency='FromCurrency', to_currency='ToCurrency')\n```\n"
         "Otherwise, reply normally.\n"
 
     ) 
--- tools.py
+++ tools.py
@@ -10,3 +10,25 @@ def get_weather(city = "kathmandu"):
             return f"Error: Unable to fetch weather data for {city}. Status code: {res.status_code}"
     except Exception as e:
         return f"weather tool error:{str(e)}"
+    
+
+def get_exchange_rate(from_currency = 'USD', to_currency = 'EUR'):
+    try:
+        url = f"https://api.frankfurter-app/latest?from={from_currency}&to={to_currency}"
+        res = requests.get(url)
+
+        if res.status_code == 200:
+            data = res.json()
+            rate = data['rates'].get(to_currency)
+
+            if rate:
+                return f"Exchange rate from {from_currency} = {rate} {to_currency}"
+            
+            else:
+                return f"Error: Unable to find exchange rate for {from_currency} to {to_currency}."
+            
+        else:
+            return f"Exchange rate API error." 
+
+    except Exception as e:
+        return f"Exchange rate tool error: {str(e)}" 
